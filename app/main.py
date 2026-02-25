5b818d70cd65b2f7c27e6147c226cc9a87bfc8ff
sim <simonas.b@hotmail.com>
2026-02-25 11:53:26 +0000

    Added saving and reading images to local storage

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-print("this be main")
+import os
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from .routes.routes import router
+from .services.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Image Modification Service")
+
+app.mount(
+    "/storage",
+    StaticFiles(directory=os.getenv("APP_STORAGE_BASE_PATH", "storage")),
+    name="storage",
+)
+app.include_router(router)
